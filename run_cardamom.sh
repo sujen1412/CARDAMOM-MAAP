3349e0823383ce6fe83f969617dccb7d80dc85e4
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-11-07 13:32:45 -0800

    adding no-hangup to task

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -7,8 +7,8 @@ apt-get install libnetcdf-dev libnetcdff-dev
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH=/opt/conda/lib:$LD_LIBRARY_PATH
 
-# Run the CARDAMOM_COMPILE.sh script
+# Run the CARDAMOM_COMPILE.sh script in the background with &
 ./BASH/CARDAMOM_COMPILE.sh
 
-# Run the CARDAMOM_MDF.exe program with the specified arguments
-./C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe DATA/CARDAMOM_DEMO_DRIVERS_prototype_1005.cbf.nc parameter.cbr
+# Run the CARDAMOM_MDF.exe program with the specified arguments in the background with &
+nohup ./C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe DATA/CARDAMOM_DEMO_DRIVERS_prototype_1005.cbf.nc parameter.cbr > cardamom_mdf_out.log &
