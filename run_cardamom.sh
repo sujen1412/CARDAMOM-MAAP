8e2d810b67748a7ecc3cbe0ba095814a012e2a80
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-11-09 08:42:36 -0800

    removing compile for testing

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -10,8 +10,8 @@ apt-get install -y libnetcdf-dev libnetcdff-dev || { echo "Failed to install req
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 
-# Run the CARDAMOM_COMPILE.sh
-"${basedir}/BASH/CARDAMOM_COMPILE.sh" || { echo "Failed to run CARDAMOM_COMPILE.sh."; exit 1; }
+# # Run the CARDAMOM_COMPILE.sh
+# "${basedir}/BASH/CARDAMOM_COMPILE.sh" || { echo "Failed to run CARDAMOM_COMPILE.sh."; exit 1; }
 
 # # Run the CARDAMOM_MDF.exe program with the specified arguments in the background with &
 # "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" DATA/CARDAMOM_DEMO_DRIVERS_prototype_1005.cbf.nc parameter.cbr || { echo "Failed to run CARDAMOM_MDF.exe."; exit 1; }
