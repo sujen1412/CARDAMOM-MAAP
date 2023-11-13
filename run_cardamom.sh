d9e3587dcfd3fe8f3d3447b5d598344828f03117
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-11-13 12:55:53 -0800

    updating script to use conda

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -5,9 +5,13 @@ set -e
 # Determine the directory of the script
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
-# Update package information and install required packages
-apt-get update
-apt-get install -y libnetcdf-dev libnetcdff-dev
+# Create and activate a Conda environment
+conda create --name myenv python=3.7
+eval "$(conda shell.bash hook)"
+conda activate myenv
+
+# Install required Conda packages
+conda install -c conda-forge libnetcdf
 
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
@@ -17,3 +21,6 @@ export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 
 # Run the CARDAMOM_MDF.exe program with the specified arguments in the background with &
 "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${basedir}/DATA/CARDAMOM_DEMO_DRIVERS_prototype_1005.cbf.nc" parameter.cbr
+
+# Deactivate the Conda environment
+conda deactivate
