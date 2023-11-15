131686d0e6398b9b88fffe0419b85f87b1f34fec
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-11-15 13:39:07 -0800

    Allowing for input files again

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -5,6 +5,14 @@ set -e
 # Determine the directory of the script
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
+# Parse command-line argument for the input file
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+fi
+
+input_file="$1"
+
 # Create and activate a Conda environment
 conda create --name myenv python=3.7
 eval "$(conda shell.bash hook)"
@@ -19,8 +27,8 @@ export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 # Run the CARDAMOM_COMPILE.sh
 "${basedir}/BASH/CARDAMOM_COMPILE.sh"
 
-# Run the CARDAMOM_MDF.exe
-"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${basedir}/DATA/CARDAMOM_DEMO_DRIVERS.cbf.nc" parameter.cbr
+# Run the CARDAMOM_MDF.exe with the provided input file
+"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "../${input_file}" parameter.cbr
 
 # Deactivate the Conda environment
 conda deactivate
