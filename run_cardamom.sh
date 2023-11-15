790710471ce0d079c31683f344df76b1cca98de5
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-11-15 12:15:34 -0800

    updating script to create env

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -6,6 +6,7 @@ set -e
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
 # Create and activate a Conda environment
+conda create --name myenv python=3.7
 eval "$(conda shell.bash hook)"
 conda activate myenv
 
