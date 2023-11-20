d8f4061e69faf69809f05b6caa5ae11275be5f91
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-11-20 14:22:38 -0800

    Ignoring error code from script

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -13,7 +13,7 @@ eval "$(conda shell.bash hook)"
 conda activate myenv
 
 # Install required Conda packages
-conda install -c conda-forge libnetcdf gcc
+conda install -c conda-forge libnetcdf gcc || true  # Ignore errors and continue
 
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
@@ -22,7 +22,7 @@ export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 "${basedir}/BASH/CARDAMOM_COMPILE.sh"
 
 # Run the CARDAMOM_MDF.exe with the provided input file
-"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" parameter.cbr
+"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" parameter.cbr || true  # Ignore errors and continue
 
 # Deactivate the Conda environment
 conda deactivate
