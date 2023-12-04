dfa4c453ea2472bcaf8a48719f846bbb9bdf45da
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-12-04 13:40:02 -0800

    refactor: keeping basic input file for parallel runs

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -5,35 +5,24 @@ set -e
 # Determine the directory of the script
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
+INPUT_FILE=$(ls -d input/*)
+
+# Create and activate a Conda environment
+conda create --name myenv python=3.7
+eval "$(conda shell.bash hook)"
+conda activate myenv
+
+# Install required Conda packages
+conda install -c conda-forge libnetcdf gcc
+
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 
 # Run the CARDAMOM_COMPILE.sh
 "${basedir}/BASH/CARDAMOM_COMPILE.sh"
 
-# Function to process a single input file
-process_input() {
-    basedir="$1"
-    input_file="$2"
-
-    echo "Processing $input_file"
-
-    # Debugging: Print current working directory
-    echo "Current working directory: $(pwd)"
-
-    # Run the command and capture output in real-time
-    "${basedir}"/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe "$input_file" parameter.cbr
-
-    # Debugging: Print a message after the command
-    echo "Done processing $input_file"
-}
-
-# Find all input files
-input_files=(input/*)
-
-# Run the processing in parallel with verbose output
-export -f process_input # Export the function for parallel
-parallel --jobs 0 --verbose process_input "$basedir" ::: "${input_files[@]}"
+# Run the CARDAMOM_MDF.exe with the provided input file
+"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" parameter.cbr
 
 # Deactivate the Conda environment
 conda deactivate
