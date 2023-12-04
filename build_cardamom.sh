0a9180a9009f3d5e590fad163b792518f9df410a
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-12-04 13:09:23 -0800

    feature: adding build script for cardamom

--- build_cardamom.sh
+++ build_cardamom.sh
@@ -5,8 +5,6 @@ set -e
 # Determine the directory of the script
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
-INPUT_FILE=$(ls -d input/*)
-
 # Create and activate a Conda environment
 conda create --name myenv python=3.7
 eval "$(conda shell.bash hook)"
@@ -17,12 +15,3 @@ conda install -c conda-forge libnetcdf gcc
 
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
-
-# Run the CARDAMOM_COMPILE.sh
-"${basedir}/BASH/CARDAMOM_COMPILE.sh"
-
-# Run the CARDAMOM_MDF.exe with the provided input file
-"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" parameter.cbr
-
-# Deactivate the Conda environment
-conda deactivate
