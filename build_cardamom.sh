a32f67a38826ff0e464c82623c27eaeb602d5dbb
ddalton <DIllon.S.Dalton@jpl.nasa.gov>
2023-12-04 13:12:06 -0800

    feature: Adding build script

--- build_cardamom.sh
+++ build_cardamom.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Determine the directory of the script
-basedir=$(cd "$(dirname "$0")" && pwd -P)
-
 # Create and activate a Conda environment
 conda create --name myenv python=3.7
 eval "$(conda shell.bash hook)"
@@ -12,6 +9,3 @@ conda activate myenv
 
 # Install required Conda packages
 conda install -c conda-forge libnetcdf gcc
-
-# Set LD_LIBRARY_PATH environment variable
-export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
