e11b696dd011a60e39c8e496681a52f777649e57
ddalton <dillon.s.dalton@jpl.nasa.gov>
2023-12-18 16:18:11 -0800

    updating output dir location

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -13,7 +13,7 @@ eval "$(conda shell.bash hook)"
 conda activate myenv
 
 # Install required Conda packages
-conda install -c conda-forge libnetcdf gcc hdf5
+conda install -c conda-forge libnetcdf gcc
 
 # Set LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
@@ -21,8 +21,11 @@ export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 # Run the CARDAMOM_COMPILE.sh
 "${basedir}/BASH/CARDAMOM_COMPILE.sh"
 
-# # Run the CARDAMOM_M.exe with the provided input file
-"${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${basedir}/parameter.cbr" "${basedir}/output/output_file.nc"
+# # Run the CARDAMOM_MDF.exe with the provided input file
+# "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" "${basedir}/parameter.cbr" || true
 
+"${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${basedir}/cbrfile.cbr" "output/output_file.nc" || true
+
+# Move outputs into ouput/ bucket
 # Deactivate the Conda environment
 conda deactivate
