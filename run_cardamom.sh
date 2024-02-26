99cff26b639515a2ba4a0bb9022d1646ca86372b
ddalton <dillon.s.dalton@jpl.nasa.gov>
2024-02-26 13:40:42 -0800

    add package installation

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -5,13 +5,23 @@ set -e
 # Determine the directory of the script
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
-# Check if the input file is provided as a command line argument
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path_to_input_file>"
-    exit 1
-fi
+INPUT_FILE=$(ls -d ../input/*)
 
-INPUT_FILE="$1"
+"${basedir}/BASH/CARDAMOM_COMPILE.sh"
+
+# Create and activate a Conda environment
+conda create --name myenv python=3.10 -y
+eval "$(conda shell.bash hook)"
+conda activate myenv
+
+# Install required Conda packages
+# Conda install commands
+conda install conda-forge::gcc conda-forge::netcdf4 conda-forge::hdf5 -y
+
+gcc --version
+
+# Set LD_LIBRARY_PATH environment variable
+export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
 
 # Run the CARDAMOM_MDF.exe with the provided input file
 # time "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" "${basedir}/output_param_file.cbr"
