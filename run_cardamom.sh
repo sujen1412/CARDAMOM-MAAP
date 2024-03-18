c3a3912ee50834cca0b01c5e9e131c580a47dfde
ddalton <dillon.s.dalton@jpl.nasa.gov>
2024-03-18 13:11:25 -0700

    refactor: add flags to CARDAMOM runscript

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -2,33 +2,34 @@
 
 set -e
 
-while getopts 'm:r:' flag; do
-    case "${flag}" in
-    m) RUN_MDF="$OPTARG" ;;
-    r) RUN_MODEL="$OPTARG" ;;
-    *)
-        echo "Usage: $0 [-m true/false] [-r true/false]" >&2
-        exit 1
-        ;;
-    esac
-done
-
+# Determine the directory of the script
 basedir=$(cd "$(dirname "$0")" && pwd -P)
 
 OUTPUTDIR="${PWD}/output"
 LOGDIR="${PWD}/logs"
 
+# Ensure the directories exist
 mkdir -p "${LOGDIR}" "${OUTPUTDIR}"
 
+# Log file common for all runs
 LOGFILE="${LOGDIR}/cardamom_run.log"
 
 log() {
     echo "$(date "+%Y-%m-%d %H:%M:%S") - $1" | tee -a "$LOGFILE"
 }
 
-"${basedir}/BASH/CARDAMOM_COMPILE.sh"
+# Default values for running components are false
+export RUN_MDF=false
+export RUN_MODEL=false
 
-export LD_LIBRARY_PATH="${basedir}:/opt/conda/lib:$LD_LIBRARY_PATH"
+# Check positional arguments
+if [[ $1 == "true" ]]; then
+  RUN_MDF=true
+fi
+
+if [[ $2 == "true" ]]; then
+  RUN_MODEL=true
+fi
 
 run_cardamom() {
     local input_file=$1
@@ -39,22 +40,22 @@ run_cardamom() {
 
     log "Starting job ${job_id} for input ${input_file}"
 
-    if [ "$RUN_MDF" = true ]; then
-        if ! "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${input_file}" "${output_param_file}" 2>&1 | tee -a "$LOGFILE"; then
-            log "Error in MDF for job ${job_id}"
-            return 1
-        fi
+    if [ "$RUN_MDF" = "true" ]; then
+      if ! time "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${input_file}" "${output_param_file}" 2>&1 | tee -a "$LOGFILE"; then
+          log "Error in MDF for job ${job_id}"
+          return 1
+      fi
 
-        log "Completed MDF for job ${job_id}"
+      log "Completed MDF for job ${job_id}"
     fi
 
-    if [ "$RUN_MODEL" = true ]; then
-        if ! "${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${input_file}" "${output_param_file}" "${output_nc_file}" 2>&1 | tee -a "${LOGFILE}"; then
-            log "Error in RUN MODEL for job ${job_id}"
-            return 1
-        fi
+    if [ "$RUN_MODEL" = "true" ]; then
+      if ! time "${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${input_file}" "${output_param_file}" "${output_nc_file}" 2>&1 | tee -a "${LOGFILE}"; then
+          log "Error in RUN MODEL for job ${job_id}"
+          return 1
+      fi
 
-        log "Completed job ${job_id}"
+      log "Completed job ${job_id}"
     fi
 }
 
@@ -62,4 +63,5 @@ export -f run_cardamom log
 
 export basedir OUTPUTDIR LOGDIR LOGFILE
 
+# Execute for all input files
 find input/ -type f | parallel -j "$(nproc)" --joblog "${LOGDIR}/parallel_joblog.log" run_cardamom {} {#} 2>&1 | tee -a "${LOGFILE}"
