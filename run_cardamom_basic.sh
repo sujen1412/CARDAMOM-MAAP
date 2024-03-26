d1b7cb185d4f0c9e378f744ef53a5d33baf4f9da
ddalton <dillon.s.dalton@jpl.nasa.gov>
2024-03-26 13:28:41 -0700

    test: add new param test file

--- run_cardamom_basic.sh
+++ run_cardamom_basic.sh
@@ -16,4 +16,4 @@ mkdir -p ${OUTPUTDIR}
 
 echo "WRITING OUTPUT NC FILE"
 
-"${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${basedir}/cbrfile.cbr" "${OUTPUTDIR}/output_file_basic.nc"
+"${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${basedir}/output_param_file.cbr" "${OUTPUTDIR}/output_file_basic.nc"
