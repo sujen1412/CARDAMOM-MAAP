cc5818e07bbe0074c14e70f9cfa43b130fca0781
ddalton <dillon.s.dalton@jpl.nasa.gov>
2024-04-01 13:29:30 -0700

    feat: add mdf run to CARDAMOM run

--- run_cardamom_basic.sh
+++ run_cardamom_basic.sh
@@ -16,4 +16,6 @@ mkdir -p ${OUTPUTDIR}
 
 echo "WRITING OUTPUT NC FILE"
 
-"${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${basedir}/output_param_file.cbr" "${OUTPUTDIR}/output_file_basic.nc"
+"${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" "${OUTPUTDIR}/output_param_file_basic.cbr"
+
+"${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${OUTPUTDIR}/output_param_file_basic.cbr" "${OUTPUTDIR}/output_file_basic.nc" || true
