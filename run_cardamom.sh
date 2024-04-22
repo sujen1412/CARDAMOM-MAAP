dcc4dbaa5bbfc0eaab9404098618c63b34922c62
ddalton <dillon.s.dalton@jpl.nasa.gov>
2024-04-22 12:48:24 -0700

    refactor: simplify run_cardamom script

--- run_cardamom.sh
+++ run_cardamom.sh
@@ -2,67 +2,21 @@
 
 set -e
 
+# shellcheck disable=SC1091
+source /opt/conda/etc/profile.d/conda.sh || true
+
+conda activate cardamom
+
 # Determine the directory of the script
-basedir=$(cd "$(dirname "$0")" && pwd -P)
-
+BASEDIR=$(cd "$(dirname "$0")" && pwd -P)
 OUTPUTDIR="${PWD}/output"
-LOGDIR="${PWD}/logs"
+INPUT_FILE=$(ls -d input/*)
+CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# Ensure the directories exist
-mkdir -p "${LOGDIR}" "${OUTPUTDIR}"
+mkdir -p "${OUTPUTDIR}"
 
-# Log file common for all runs
-LOGFILE="${LOGDIR}/cardamom_run.log"
+"${BASEDIR}/BASH/CARDAMOM_COMPILE.sh"
 
-log() {
-    echo "$(date "+%Y-%m-%d %H:%M:%S") - $1" | tee -a "$LOGFILE"
-}
+"${BASEDIR}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${INPUT_FILE}" "${OUTPUTDIR}/output_param_file_${CURRENT_TIME}.cbr" || true
 
-# Default values for running components are false
-export RUN_MDF=false
-export RUN_MODEL=false
-export IS_TEST_RUN=false
-
-# Check positional arguments
-if [[ $1 == "true" ]]; then
-    RUN_MDF=true
-fi
-
-if [[ $2 == "true" ]]; then
-    RUN_MODEL=true
-fi
-
-if [[ $3 == "true" ]]; then
-    IS_TEST_RUN=true
-fi
-
-run_cardamom() {
-    local input_file=$1
-    local job_id=$2
-    local base_name=$(basename "${input_file}" | sed 's/\(.*\)\..*/\1/')
-    local output_param_file="${OUTPUTDIR}/output_param_${base_name}.cbr"
-    local output_nc_file="${OUTPUTDIR}/output_file_${base_name}.nc"
-
-    log "Starting job ${job_id} for input ${input_file}"
-
-    if [ "$RUN_MDF" = "true" ]; then
-        time "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${input_file}" "${output_param_file}" 2>&1 | tee -a "$LOGFILE"
-        log "Completed MDF for job ${job_id}"
-    fi
-
-    if [ "$RUN_MODEL" = "true" ]; then
-        "${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${input_file}" "${output_param_file}" "${output_nc_file}" 2>&1 | tee -a "${LOGFILE}"
-        log "Completed job ${job_id}"
-    fi
-
-    if [ "$IS_TEST_RUN" = "true" ]; then
-        time "${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${basedir}/input/PROJSCRIPT_CARDAMOM_CMSFLUX_PRIORS_JUL23_V1_1942.cbf.nc" "${basedir}/cbrfile.cbr" "${OUTPUTDIR}/output_file_test.nc" 2>&1 | tee -a "${LOGFILE}"
-    fi
-}
-
-export -f run_cardamom log
-
-export basedir OUTPUTDIR LOGDIR LOGFILE
-
-# Execute for all input files
-find input/ -type f | parallel -j "$(nproc)" --joblog "${LOGDIR}/parallel_joblog.log" run_cardamom {} {#} 2>&1 | tee -a "${LOGFILE}"
+"${BASEDIR}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${INPUT_FILE}" "${OUTPUTDIR}/output_param_file_${CURRENT_TIME}.cbr" "${OUTPUTDIR}/output_file_${CURRENT_TIME}.nc" || true
