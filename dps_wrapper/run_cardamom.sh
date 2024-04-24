46c6eae4f016f1268f25bf2781a0fd71517902d0
ddalton <dillon.s.dalton@jpl.nasa.gov>
2024-04-24 09:51:42 -0700

    refactor: move the run script into the dps_wrapper

--- dps_wrapper/run_cardamom.sh
+++ dps_wrapper/run_cardamom.sh
@@ -20,12 +20,6 @@ mkdir -p output
 input_filename=$(ls -d input/*)
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 
-conda run --live-stream --name vanilla "${basedir}/BASH/CARDAMOM_COMPILE.sh"
-
-echo "Running CARDAMOM MDF"
-
-conda run --live-stream --name vanilla "${basedir}/C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${input_filename}" "output/output_param_file_${current_time}.cbr"
-
-echo "Running CARDAMOM MODEL"
-
-conda run --live-stream --name vanilla "${basedir}/C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${input_filename}" "output/output_param_file_${current_time}.cbr" "output/output_file_${current_time}.nc"
+conda run --live-stream --name vanilla "${basedir}/../BASH/CARDAMOM_COMPILE.sh"
+conda run --live-stream --name vanilla "${basedir}/../C/projects/CARDAMOM_MDF/CARDAMOM_MDF.exe" "${input_filename}" "output/output_param_file_${current_time}.cbr"
+conda run --live-stream --name vanilla "${basedir}/../C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.exe" "${input_filename}" "output/output_param_file_${current_time}.cbr" "output/output_file_${current_time}.nc"
